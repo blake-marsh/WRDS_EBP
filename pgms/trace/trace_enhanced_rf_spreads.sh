e6f2c3dab20b555ef8801d13b0dc5bebe447b74f
Blake Marsh <j1wbm01@wrds-cloud-login1-w.wharton.private>
2025-07-29 17:13:46 -0400

    adding files for trace

--- pgms/trace/trace_enhanced_rf_spreads.sh
+++ pgms/trace/trace_enhanced_rf_spreads.sh
@@ -5,29 +5,27 @@
 #$ -N trace_enhanced
 #$ -pe onenode 8
 #$ -l m_mem_free=6G
-#$ -o $HOME/WRDS_EBP/unrealized-gains/logfiles/trace_enhanced.o.log
-#$ -e $HOME/WRDS_EBP/unrealized-gains/logfiles/trace_enhanced.e.log
+#$ -o $HOME/WRDS_EBP/logfiles/trace_enhanced.o.log
+#$ -e $HOME/WRDS_EBP/logfiles/trace_enhanced.e.log
 
 
-cd ~/WRDS_EBP/unrealized-gains/
+cd ~/WRDS_EBP/pgms/trace/
 
-
-# download the H15 data
-#source ~/virtualenv/base_python/bin/activatesource ~/virtualenv/base_python/bin/activate
-#python ./pgms/download_h15.py
-#R CMD BATCH --no-save --no-restore ./pgms/format_h15.R ./logfiles/format_h15.Rout
-#deactivate
-
-#R CMD BATCH --no-save --no-restore ./pgms/y9c_cusip_match.R ./logfiles/y9c_cusip_match.Rout
+# download Treasury rates from the Federal Reserve's H.15 release
+source ~/virtualenv/base_python/bin/activatesource ~/virtualenv/base_python/bin/activate
+python ./pgms/download_h15.py
+R CMD BATCH --no-save --no-restore ./pgms/format_h15.R ./logfiles/format_h15.Rout
+deactivate
 
 ## get trace data and process
-#R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_query.R ./logfiles/trace_enhanced_query.Rout
-#R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_debt_types.R ./logfiles/trace_enhanced_debt_types.Rout
+R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_query.R ./logfiles/trace_enhanced_query.Rout
+R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_debt_types.R ./logfiles/trace_enhanced_debt_types.Rout
 
-#R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_with_fisd_characteristics.R ./logfiles/trace_enhanced_with_fisd_characteristics.Rout
+## bond characteristics from FISD mergent
+R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_with_fisd_characteristics.R ./logfiles/trace_enhanced_with_fisd_characteristics.Rout
 
-##R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_add_bbg_characteristics.R ./logfiles/trace_enhanced_add_bbg_characteristics.Rout
-#R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_sample.R ./logfiles/trace_enhanced_sample.Rout
+## determine the trace sample
+R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_sample.R ./logfiles/trace_enhanced_sample.Rout
 
 ## calculate the risk-free spreads off synthetic Treasuries
 R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_rf_spreads.R ./logfiles/trace_enhanced_rf_spreads.Rout
