def013ebaa79ee858889be388bb651ca52a6287d
Blake Marsh <j1wbm01@wrds-cloud-login1-w.wharton.private>
2025-07-30 11:06:38 -0400

    fixing file paths

--- pgms/trace/trace_enhanced_rf_spreads.sh
+++ pgms/trace/trace_enhanced_rf_spreads.sh
@@ -9,26 +9,26 @@
 #$ -e $HOME/WRDS_EBP/logfiles/trace_enhanced.e.log
 
 
-cd ~/WRDS_EBP/pgms/trace/
+cd ~/WRDS_EBP/
 
 # download Treasury rates from the Federal Reserve's H.15 release
 source ~/virtualenv/base_python/bin/activatesource ~/virtualenv/base_python/bin/activate
-python ./pgms/download_h15.py
-R CMD BATCH --no-save --no-restore ./pgms/format_h15.R ./logfiles/format_h15.Rout
+python ./pgms/trace/download_h15.py
+R CMD BATCH --no-save --no-restore ./pgms/trace/format_h15.R ./logfiles/format_h15.Rout
 deactivate
 
 ## get trace data and process
-R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_query.R ./logfiles/trace_enhanced_query.Rout
-R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_debt_types.R ./logfiles/trace_enhanced_debt_types.Rout
+R CMD BATCH --no-save --no-restore ./pgms/trace/trace_enhanced_query.R ./logfiles/trace_enhanced_query.Rout
+#R CMD BATCH --no-save --no-restore ./pgms/trace/trace_enhanced_debt_types.R ./logfiles/trace_enhanced_debt_types.Rout
 
 ## bond characteristics from FISD mergent
-R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_with_fisd_characteristics.R ./logfiles/trace_enhanced_with_fisd_characteristics.Rout
+#R CMD BATCH --no-save --no-restore ./pgms/trace/trace_enhanced_with_fisd_characteristics.R ./logfiles/trace_enhanced_with_fisd_characteristics.Rout
 
 ## determine the trace sample
-R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_sample.R ./logfiles/trace_enhanced_sample.Rout
+#R CMD BATCH --no-save --no-restore ./pgms/trace/trace_enhanced_sample.R ./logfiles/trace_enhanced_sample.Rout
 
 ## calculate the risk-free spreads off synthetic Treasuries
-R CMD BATCH --no-save --no-restore ./pgms/trace_enhanced_rf_spreads.R ./logfiles/trace_enhanced_rf_spreads.Rout
+#R CMD BATCH --no-save --no-restore ./pgms/trace/trace_enhanced_rf_spreads.R ./logfiles/trace_enhanced_rf_spreads.Rout
 
 
 
