3af4c4ef5e452f96a098c77b42ec6a3375d5a46a
j1wbm01 <blake.marsh@kc.frb.org>
2025-08-05 20:19:48 -0500

    updating file paths for h15 formatting

--- pgms/trace/format_h15.R
+++ pgms/trace/format_h15.R
@@ -3,10 +3,10 @@ rm(list = ls())
 library(data.table)
 library(zoo)
 
-setwd("/scratch/frbkc/WRDS_EBP/")
+setwd("~/WRDS_EBP/")
 
 ## load the h15 data
-h15 = fread("H15_B.csv", sep=",", stringsAsFactors=F)
+h15 = fread("./data/H15_B.csv", sep=",", stringsAsFactors=F)
 h15[,date := as.Date(TIME_PERIOD, format='%Y-%m-%d')]
 
 ## convert h15 to decimals
@@ -19,7 +19,7 @@ h15_names_dec = c('tyd00m', 'tyd01m', 'tyd03m', 'tyd06m', 'tyd01y',
 h15[,(h15_names_dec) := lapply(.SD, function(x) as.numeric(x)/100), .SDcols=h15_names]
 
 ## keep subset of the data
-h15 = h15[which(date >= as.Date('2015-01-01')),list(date, tyd00m, tyd01m, tyd03m, tyd06m, tyd01y,
+h15 = h15[which(date >= as.Date('2000-01-01')),list(date, tyd00m, tyd01m, tyd03m, tyd06m, tyd01y,
                                                                   tyd02y, tyd03y, tyd05y, tyd07y,
                                                                   tyd10y, tyd20y, tyd30y)]
 
@@ -27,5 +27,5 @@ h15 = h15[which(date >= as.Date('2015-01-01')),list(date, tyd00m, tyd01m, tyd03m
 h15 = h15[complete.cases(h15),]
 
 # Save the data
-h15 = saveRDS(h15, "H15.rds")
+saveRDS(h15, "./data/H15.rds")
 
